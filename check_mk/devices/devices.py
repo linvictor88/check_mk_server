15db73e62b53d65ea2a814f258f8e1bb61156c24
linvictor88 <linb@vmware.com>
2014-01-06 09:10:46 +0800

    Add System and disks collector functions.

--- check_mk/devices/devices.py
+++ check_mk/devices/devices.py
@@ -1,6 +1,8 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 import logging
+import re
+import time
 
 from check_mk.agent.linux import utils
 from check_mk.devices import abstract_device
@@ -56,10 +58,7 @@ class Memory(abstract_device.AbstractDevice):
         self.active = device_dict['active']
 
 class Cpu(abstract_device.AbstractDevice):
-    """Cpu device data collector.
-
-    All variables are in units of KB.
-    """
+    """Cpu device data collector."""
 
     name = 'cpu'
     def get_plain_info(self):
@@ -120,3 +119,92 @@ class Cpu(abstract_device.AbstractDevice):
         self.softirq = device_dict['softirq']
         self.steal = device_dict['steal']
         self.total = device_dict['total']
+
+
+class System(abstract_device.AbstractDevice):
+    """System device data collector."""
+
+    name = 'system'
+    def get_plain_info(self):
+        """Get plain info of system."""
+        cmd = ['cat', '/proc/uptime']
+        plain_info = [line.split(' ', 1) 
+                      for line in utils.execute(cmd).split('\n')
+                      if line]
+        return plain_info
+
+    def parse_plain_info(self, plain_info):
+        self.uptime = int(plain_info[0][0])
+
+    def get_device_dict(self):
+            return {'uptime': self.uptime}
+
+    def init_device(self, device_dict):
+        self.uptime = device_dict['uptime']
+
+class Disks(abstract_device.AbstractDevice):
+    """Disk device data collector."""
+
+    name = 'disks'
+    def get_plain_info(self):
+        """Get plain info of system."""
+        excludefs="-x smbfs -x tmpfs -x devtmpfs -x cifs -x iso9660 -x udf -x nfsv4 -x nfs -x mvfs -x zfs"
+        df_cmd = ['df', '-PTlk', excludefs]
+        df_info = [line.split() 
+                   for line in utils.execute(df_cmd).split('\n')
+                   if line]
+        del df_info[0]
+        for line in df_info:
+            if os.path.islink(line[0]):
+                realpath = os.path.realpath(line[0])
+                line[0] = os.path.basename(realpath)
+            mapping_df[line[0]] = line
+
+        disk_cmd = ['cat', '/proc/diskstats']
+        p = re.compile('x?[shv]d[a-z]*|cciss/c[0-9]+d[0-9]+|emcpower[a-z]+|dm-[0-9]+|VxVM.*')
+        disk_info = [line.split() 
+                     for line in utils.execute(disk_cmd).split('\n')
+                     if line and p.search(line)]
+        for line in disk_info:
+            mapping_disk[line[2]] = map(lambda x: int(x), line[:2]+line[3:14])
+            mapping_disk[line[2]].insert(2, line[2])
+
+        for k, v in mapping_disk.items():
+            if k in mapping_df.keys():
+                mapping_info[k] = mapping_disk[k] + mapping_df[k]
+        return mapping_info
+
+    def parse_plain_info(self, plain_info):
+        self.count = len(plain_info)
+        self.disks = []
+        for name, v in plain_info:
+            disk = {'name': name}
+            disk['readTput'] = v[5] 
+            disk['writeTput'] =  v[9]
+            disk['iops'] = v[3] + v[7]
+            disk['latency'] = v[12]
+            disk['total'] = v[15]
+            disk['usage'] = v[18]
+            disks.append(disk)
+        
+    def get_device_dict(self):
+            return {'count': self.count,
+                    'disks': self.disks}
+
+    def init_device(self, device_dict):
+        self.time = device_dict['time']
+        self.count = device_dict['count']
+        self.disks = device_dict['disks']
+
+    def write_device_file(self, hostname):
+        """Write device data into file."""
+        for disk in self.disks:
+            device_dir = os.path.join(DATA_BASE_DIR, hostname, self.name, disk['name'])
+            if not os.path.isdir(device_dir):
+                os.makedirs(device_dir, 0o755)
+            timestamp = int(time.time())
+            for k, v in self.get_device_dict().items():
+                file_name = os.path.join(device_dir, k)
+                data = _("%(timestamp)d %(value)s\n" % {'timestamp': timestamp,
+                                                  'value': v})
+                utils.write_file(file_name, data)  
